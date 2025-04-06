b89eea5f7bd1133659f8a980618c79fb07d46d39
e1q23079 <e1q23079@oit.ac.jp>
2025-04-06 21:34:11 +0900

    setup

--- server.js
+++ server.js
@@ -71,6 +71,31 @@ app.post('/registration', (req, res) => {
     //console.log(req.body);
 });
 
+// セットアップ（GET）
+app.get('/setup/:pass', (req, res) => {
+    if (req.params.pass === process.env.PASS) {
+        try {
+            const sql1 = "CREATE TABLE `record` (`id` int(11) NOT NULL,`time` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+            connection.query(sql1, (error, response) => {
+                
+            });
+            const sql2 = "ALTER TABLE `record` ADD PRIMARY KEY (`id`);";
+            connection.query(sql2, (error, response) => {
+                
+            });
+            const sql3 = "ALTER TABLE `record` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;";
+            connection.query(sql3, (error, response) => {
+                
+            });
+            res.send("Success");
+        } catch {
+            res.send("Already OK");
+        }
+    } else {
+        res.send("No Permission");
+    }
+});
+
 
 app.listen(port, () => {
     console.log("Server is running!!");
