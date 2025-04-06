bd21e54ce54261bfc5be861a01f4b46bbd73768f
e1q23079 <e1q23079@oit.ac.jp>
2025-04-06 17:10:08 +0900

    recapturetapper

---
+++ README.md
@@ -0,0 +1,58 @@
+# ReCapture-Tapper
+```
+npm run start
+```
+## 環境変数
+- HOST
+- USER
+- PASSWORD
+- DATABASE
+## データベース
+```
+--
+-- データベース: `gameRecord`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `record`
+--
+
+CREATE TABLE `record` (
+  `id` int(11) NOT NULL,
+  `time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `time`
+--
+--
+-- INSERT INTO `time` (`id`, `time`) VALUES
+-- (1, 12);
+--
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `time`
+--
+ALTER TABLE `time`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルのAUTO_INCREMENT
+--
+
+--
+-- テーブルのAUTO_INCREMENT `time`
+--
+ALTER TABLE `time`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+```
---
+++ server.js
@@ -0,0 +1,77 @@
+// Expressを利用
+const express = require('express');
+// MySQLを利用
+const mysql = require('mysql2');
+// ポート番号を指定
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// SQLへの接続情報
+const connection = mysql.createConnection({
+    host: process.env.HOST, // 環境変数：HOST
+    user: process.env.USER, // 環境変数：USER
+    password: process.env.PASSWORD, // 環境変数：PASSWORD
+    database: process.env.DATABASE  // 環境変数：DATABASE
+});
+
+// データベースへ接続
+connection.connect();
+
+// JSON形式に設定
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// publicフォルダーを公開
+app.use(express.static('public'));
+
+// timeを指定して，ランキングを取得（GET）
+app.get('/getRank/:time', (req, res) => {
+    connection.query('SELECT DISTINCT time FROM time ORDER BY time;', (error, response) => {
+        if (error) throw error;
+        //console.log(response);
+        //res.json(response);
+        //console.log(req.params.time);
+        let rank = "データなし";
+        for (let i in response) {
+            //console.log(response[i].time);
+            if (response[i].time == req.params.time) {
+                rank = parseInt(i) + 1;
+            }
+        }
+        const rankJson = [{ rank: rank }]
+        res.json(rankJson);
+    });
+});
+
+// 上位100名のランキングリストを取得（GET）
+app.get('/getRankList', (req, res) => {
+    connection.query('SELECT DISTINCT time FROM time ORDER BY time LIMIT 100;', (error, response) => {
+        if (error) throw error;
+        //console.log(response);
+        res.json(response);
+    });
+});
+
+// 登録されているデータをすべて取得（GET）
+app.get('/showList', (req, res) => {
+    connection.query('SELECT time FROM time ORDER BY time;', (error, response) => {
+        if (error) throw error;
+        //console.log(response);
+        res.json(response);
+    });
+});
+
+// 時刻を記録する（POST）
+app.post('/registration', (req, res) => {
+    connection.query(`INSERT INTO time(time) VALUES (${req.body.time});`, (error, response) => {
+        if (error) throw error;
+    });
+    res.send(req.body);
+    //console.log(req.body);
+});
+
+
+app.listen(port, () => {
+    console.log("Server is running!!");
+});
